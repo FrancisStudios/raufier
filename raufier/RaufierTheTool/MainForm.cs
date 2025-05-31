d5d1ace5b19717ea68e5cd29479009e148701ba5
Francis <vlfmap@gmail.com>
2025-05-31 20:05:27 +0200

    project setup

---
+++ raufier/RaufierTheTool/MainForm.cs
@@ -0,0 +1,31 @@
+﻿/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ===========================
+ * github: https://github.com/francisstudios
+ * ©2025 Francis Studios Softwares by L.
+ * 
+ * Project Started on 2025.05.31 | 19:56 BP, HU
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RaufierTheTool
+{
+	public partial class Ablak : Form
+	{
+		public Ablak()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ raufier/RaufierTheTool/Program.cs
@@ -0,0 +1,26 @@
+﻿/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ===========================
+ * github: https://github.com/francisstudios
+ * ©2025 Francis Studios Softwares by L.
+ * 
+ * Project Started on 2025.05.31 | 19:56 BP, HU
+*/
+
+using System;
+using System.Windows.Forms;
+
+namespace RaufierTheTool
+{
+	internal sealed class Program
+	{
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Ablak());
+		}
+		
+	}
+}
